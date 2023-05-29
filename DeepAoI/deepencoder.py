09920f95f1f715b46788d26c07a44aae61363e78
cahthuranag <chathurangab@sltc.ac.lk>
2023-05-29 11:35:16 +0100

    deepencoder

--- DeepAoI/deepencoder.py
+++ DeepAoI/deepencoder.py
@@ -1,9 +1,12 @@
 def deepencoder (n,k,snr):
- #this function calculates the block error rate for the given SNR, n, k.
-    # n is the number of bits in the block
-     # k is the number of bits in the message
-  # snr is the signal to noise rati
- # importing libs
+ """Deep encoder for AWGN channel
+ Args:
+ n (int): Number of bits in a block.
+ k (int): Number of bits in a message.
+ snr (float): SNR in linear scale.
+ Returns:
+ simulation AAoI.
+    """
  import numpy as np
  import tensorflow as tf
  import keras
@@ -13,75 +16,46 @@ def deepencoder (n,k,snr):
  from tensorflow.keras.layers import BatchNormalization
  from keras.optimizers import Adam,SGD
  from keras import backend as K
- # for reproducing reslut
  import numpy as np
  import tensorflow as tf
- 
- # Set random seed for NumPy
  np.random.seed(1)
- 
- # Set random seed for TensorFlow
  tf.random.set_seed(3)
- # defining parameters
- # define (n,k) here for (n,k) autoencoder
- # n = n_channel 
- # k = log2(M)  ==> so for (7,4) autoencoder n_channel = 7 and M = 2^4 = 16 
- 
  M=2**k
- n_channel = n
- R = k/n_channel
- print ('M:',M,'k:',k,'n:',n_channel)
- #generating data of size N
+ R = k/n
+ print ('M:',M,'k:',k,'n:',n)
  N = 800
  label = np.random.randint(M,size=N)
- # creating one hot encoded vectors
  data = []
  for i in label:
      temp = np.zeros(M)
      temp[i] = 1
      data.append(temp)
- # checking data shape
  data = np.array(data)
  print (data.shape)
- # checking generated data with it's label
  temp_check = [17,23,45,67,89,96,72,250,350]
  for i in temp_check:
      print(label[i],data[i])
- # defining autoencoder and it's layer
  input_signal = Input(shape=(M,))
  encoded = Dense(M, activation='relu')(input_signal)
- encoded1 = Dense(n_channel, activation='linear')(encoded)
- encoded2 = Lambda(lambda x: np.sqrt(n_channel)*K.l2_normalize(x,axis=1))(encoded1)
- 
- EbNo_train = 5.01187 #  coverted 7 db of EbNo
- encoded3 = GaussianNoise(np.sqrt(1/(2*R*EbNo_train)))(encoded2)
+ encoded1 = Dense(n, activation='linear')(encoded)
+ encoded2 = Lambda(lambda x: np.sqrt(n)*K.l2_normalize(x,axis=1))(encoded1)
+ snr_train = 5.01187 #  coverted 7 db of EbNo
+ encoded3 = GaussianNoise(np.sqrt(1/(2*R*snr_train)))(encoded2)
  
  decoded = Dense(M, activation='relu')(encoded3)
  decoded1 = Dense(M, activation='softmax')(decoded)
  autoencoder = Model(input_signal, decoded1)
  adam = Adam(lr=0.01)
  autoencoder.compile(optimizer=adam, loss='categorical_crossentropy')
- # printing summary of layers and it's trainable parameters 
- print (autoencoder.summary())
- # traning auto encoder
  autoencoder.fit(data, data,
                  epochs=100,
                  batch_size=32)
- # saving keras model
  from keras.models import load_model
- # if you want to save model then remove below comment
- # autoencoder.save('autoencoder_v_best.model')
- # # making encoder from full autoencoder
  encoder = Model(input_signal, encoded2)
- # making decoder from full autoencoder
- encoded_input = Input(shape=(n_channel,))
- 
+ encoded_input = Input(shape=(n,))
  deco = autoencoder.layers[-2](encoded_input)
  deco = autoencoder.layers[-1](deco)
  decoder = Model(encoded_input, deco)
- # generating data for checking BER
- # if you're not using t-sne for visulation than set N to 70,000 for better result 
- # for t-sne use less N like N = 1500
  N = 50000
  test_label = np.random.randint(M,size=N)
  test_data = []
@@ -89,41 +63,19 @@ def deepencoder (n,k,snr):
  for i in test_label:
      temp = np.zeros(M)
      temp[i] = 1
-     test_data.append(temp)
-     
+     test_data.append(temp)    
  test_data = np.array(test_data)
  # checking generated data
  temp_test = 6
- print (test_data[temp_test][test_label[temp_test]],test_label[temp_test])
- def frange(x, y, jump):
-   while x < y:
-     yield x
-     x += jump
- # calculating BER
- # this is optimized BER function so it can handle large number of N
- # previous code has another for loop which was making it slow
- #EbNodB_range = list(frange(0))
- #ber = [None]*len(EbNodB_range)
- #for n in range(0,len(EbNodB_range)):
- #EbNo=10.0**(EbNodB_range[n]/10.0)
-
- EbNo=snr
- noise_std = np.sqrt(1/(2*R*EbNo))
- noise_mean = 0
+ noise_std = np.sqrt(1/(2*R*snr))
  no_errors = 0
- nn = N
- noise = noise_std * np.random.randn(nn,n_channel)
+ noise = noise_std * np.random.randn(N,n)
  encoded_signal = encoder.predict(test_data) 
  final_signal = encoded_signal + noise
  pred_final_signal =  decoder.predict(final_signal)
  pred_output = np.argmax(pred_final_signal,axis=1)
  no_errors = (pred_output != test_label)
  no_errors =  no_errors.astype(int).sum()
- ber= no_errors / nn 
- return ber
-
-
-#print ('SNR:',SNR,'BER:',ber)
-    # use below line for generating matlab like matrix which can be copy and paste for plotting ber graph in matlab
-    #print(ber[n], " ",end='')    
+ ber= no_errors / N
+ return ber   
 print(deepencoder(7,4,0.015))        
--- DeepAoI/maincom.py
+++ DeepAoI/maincom.py
@@ -11,8 +11,6 @@ def main(
 
     from av_age import average_age_of_information_fn 
     from snr import snr_th 
-    #from agenet.bler import blercal, blercal_th
-    #from agenet.bler import blercal_
     from deepencoder import deepencoder
     """
     Simulates a communication system and calculates the AAoI.
@@ -23,11 +21,9 @@ def main(
         n (int): Number of bits in a block.
         k (int): Number of bits in a message.
         P (float): Power of the nodes.
-        numevents (int): Number of events to simulate.
 
     Returns:
-        Tuple[float, float]: A tuple of two floats.
-        theoretical AAoI amd simulation AAoI.
+    simulation AAoI.
     """
     lambda1 = 1  # arrival for one transmission period
     # lambda1 = genlambda[j]
@@ -36,45 +32,17 @@ def main(
         (np.ones(num_events))  # inter arrival times
     arrival_timestamps = np.cumsum(inter_arrival_times)  # arrival timestamps
     N0 = 1 * (10**-11)  # noise power
-    d1 = 700  # disatance between source nodes and relay 
-    d2 = 700  # distance between the relay and destination
-    P1 = P  # power of the source nodes
-    P2 = P  # power of the relay or access point
-    # n = 300
-    n1 = n  # number of bits in the block for the source nodes
-    n2 = n  # number of bits in the block for the relay or access point
-    k1 = k  # number of bits in the message for the source nodes
-    k2 = k  # number of bits in the message for the relay or access point
-    # block error rate for the relay or access point at the destination
-    snr1_th = snr_th(N0, d1, P1)
-    #snr2_th = snr.snr_th(N0, d2, P2)
-    #er1_th = blercal_th(snr1_th, n1, k1)
-    #er2_th = blercal_th(snr2_th, n2, k2)
+    d1 = 700  # disatance between source nodes and destination
+    snr1_th = snr_th(N0, d1, P)
     inter_service_times = (1 / lambda1) * \
         np.ones((num_events))  # inter service times
     # Generating departure timestamps for the node 1
     server_timestamps_1 = np.zeros(num_events)
     departure_timestamps_s = np.zeros(num_events)
-    #su_p_th = active_prob * (1 - er1_th) * \
-    #  ((1 - active_prob) ** (num_nodes - 1))
-    #er_f_th = 1 - su_p_th
-    # er_p_th = er_f_th + (er2_th * (er_f_th - 1))
-    er_p_1=deepencoder(n1,k1,snr1_th)
-    print(er_p_1)
+    er_p_1=deepencoder(n,k,snr1_th)
+   # print(er_p_1)
+
     for i in range(0, num_events):
-        #snr1 = snr.snr(
-          #  N0, d1, P1
-        #)  snr for the source nodes at the relay or access point
-        #snr2 = snr.snr(N0, d2, P2)
-       # er1 = blercal(
-          #  snr1, n1, k1
-        #)  # block error rate for the source nodes at the relay or access point
-        #er2 = blercal(
-            #snr2, n2, k2
-        #)  # block error rate for the relay or access point at the destination
-        #su_p = active_prob * (1 - er1) * ((1 - active_prob) ** (num_nodes - 1))
-        #er_f = 1 - su_p
-        #er_p = autoencoder_er_p(er_p_th, active_prob)
         er_p = 1- (active_prob * (1 - er_p_1) * ((1 - active_prob) ** (num_nodes - 1)))
         er_indi = int(random.random() > er_p)
         if er_indi == 0:
@@ -106,20 +74,10 @@ def main(
             t1 = [0]
     else:
         t1 = [0] + sermat
-    # if np.size(dep) == 1 or np.size(sermat) == 1:
-    # dep.extend([arrival_timestamps[-1]])
-    # sermat= [arrival_timestamps[1]]+ sermat
-
-    # print(sermat, dep)
+  # print(sermat, dep)
     system_time = 1 / lambda1  # system time (time which update in the system)
     av_age_simulation, _, _ = average_age_of_information_fn(
         v1, t1, system_time)
-
-    # print(er1, er2, er1_th, er2_th)
-   # av_age_theoretical = (1 / lambda1) * (0.5 + (1 / (1 - er_p_th)))
-
-    # print(av_age_simulation, av_age_theoretical)
-   # return av_age_theoretical, av_age_simulation
     return av_age_simulation
 
 def plot_av_age_simulation(num_nodes, active_prob, n, k, P_range):
@@ -130,7 +88,7 @@ def plot_av_age_simulation(num_nodes, active_prob, n, k, P_range):
         av_age_simulations_ls.append(av_age_sim)
 
     plt.plot(P_range, av_age_simulations_ls, marker='o')
-    plt.xlabel('Block Length (n)')
+    plt.xlabel('Transmission Power')
     plt.ylabel('Average Age of Information')
     plt.title('Average Age of Information vs Transmission Power')
     plt.grid(True)
@@ -142,7 +100,7 @@ num_nodes = 2
 active_prob = 0.5
 n = 7
 k = 4
-P_range = [0.01, 0.05, 0.1, 0.5, 1, 5, 10, 50]
+P_range = [0.01, 0.05, 0.1, 0.5, 1, 5, 10]
 
 plot_av_age_simulation(num_nodes, active_prob, n, k, P_range)
 
